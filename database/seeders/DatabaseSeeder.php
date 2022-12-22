cd99092299a4a7d85994f5540b41fb4101af8b79
belino93 <abelmadrid93@gmail.com>
2022-12-22 16:30:31 +0100

    repairs seeded

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         $this -> call(RoleSeeders::class);
         $this -> call(UserSeeder::class);
+        $this -> call(RepairSeeders::class);
     }
 }
--- database/seeders/RepairSeeders.php
+++ database/seeders/RepairSeeders.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class RepairSeeders extends Seeder
 {
@@ -14,6 +16,15 @@ class RepairSeeders extends Seeder
      */
     public function run()
     {
-        //
+        Log::info('Seed repairs');
+        DB::table('repairs')->insert([
+            ['tipo' => 'Cambio de pantalla'],
+            ['tipo' => 'Cambio de conector de carga'],
+            ['tipo' => 'Cambio altavoz superior'],
+            ['tipo' => 'Cambio altavoz inferior'],
+            ['tipo' => 'Cambio de bateria'],
+            ['tipo' => 'Cambio camara trasera'],
+            ['tipo' => 'Cambio camara frontal'],
+        ]);
     }
 }
