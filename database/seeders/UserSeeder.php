55cc017b71cdb19835e94bcfeb5ca68ec3c4346f
belino93 <abelmadrid93@gmail.com>
2022-12-22 14:55:44 +0100

    user seeds

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +16,26 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Log::info('Seed users');
+        DB::table('users')->insert([
+            [
+                'name'=>'Fran',
+                'surname'=>'Cisco',
+                'email'=>'fran@gmail.com',
+                'password'=>'pass1234',
+            ],
+            [
+                'name'=>'Almu',
+                'surname'=>' Dena',
+                'email'=>'almu@gmail.com',
+                'password'=>'pass1234',
+            ],
+            [
+                'name'=>'Abel',
+                'surname'=>'Madrid',
+                'email'=>'abel@gmail.com',
+                'password'=>'pass1234',
+            ]
+            ]);
     }
 }
