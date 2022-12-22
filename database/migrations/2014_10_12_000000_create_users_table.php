9c71120507292394caa66b9d30a969f8285616e6
belino93 <abelmadrid93@gmail.com>
2022-12-22 12:17:40 +0100

    add relationship between users-role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,11 @@ return new class extends Migration
             $table->string('surname');
             $table->string('email')->unique();          
             $table->string('password');
+            $table->unsignedBigInteger('role_id')->default(1);
+            $table->boolean('active')->default(true);
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
     }
 
