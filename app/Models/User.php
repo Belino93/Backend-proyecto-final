90ec74fb32ae8a5f1ea9ab7a824512e952a045e4
belino93 <abelmadrid93@gmail.com>
2022-12-24 12:11:32 +0100

    Create user model with role relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    protected function role()
+    {
+        $this->hasOne(User::class, 'role_id');
+    }
 }
