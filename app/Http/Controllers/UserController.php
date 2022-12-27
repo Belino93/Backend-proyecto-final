8a59196bc4eb5cb7f61fb2363a99f64c9046d53f
belino93 <abelmadrid93@gmail.com>
2022-12-27 21:31:52 +0100

    feat: Update user created

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,18 +5,20 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
+    // Get all users(admin)
     public function getUsers()
     {
         Log::info('Getting user');
         try {
             $users = User::all()->toArray();
             return response([
-                'success'=> true,
-                'message'=> 'Users retrieving successfully',
-                'data'=> $users
+                'success' => true,
+                'message' => 'Users retrieving successfully',
+                'data' => $users
             ]);
         } catch (\Throwable $th) {
             Log::error($th->getMessage());
@@ -24,6 +26,46 @@ class UserController extends Controller
             return response([
                 'success' => false,
                 'message' => 'Get users fails',
+            ], 400);
+        }
+    }
+
+    public function updateUser(Request $request)
+    {
+        Log::info('Updating userName');
+        $userId = auth()->user()->id;
+        
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|max:100|string',
+                'surname' => 'required|max:100|string'
+
+            ]);
+
+            if ($validator->fails()) {
+                return response([
+                    'success' => false,
+                    'message' => $validator->messages()
+                ], 400);
+            }
+
+            $user = User::find($userId);
+            $user->name = $request->input('name');
+            $user->surname = $request->input('surname');
+            $user->save();
+
+            return response([
+                "success"=>true,
+                'message'=>'Username updated',
+                'data'=> [$user->name, $user->surname]
+            ], 200); 
+
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+
+            return response([
+                'success' => false,
+                'message' => 'Update user fails',
             ], 400);
         }
     }
